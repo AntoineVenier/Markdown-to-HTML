aaadb0e39d2d6e485429a479b759d42a855dc651
Antoine Venier <antoine.venier@hotmail.com>
2018-12-23 14:45:32 +0100

    prise d'agurment et affichage des arguments

--- GenerateurSiteStatique.py
+++ GenerateurSiteStatique.py
@@ -1,7 +1,9 @@
 import argparse 
 
 arguments_passes=argparse.ArgumentParser()#On stocke dans arguments_passes les arguments passes au programme
-#Message d aide pour chaque options
-arguments_passes.add_argument("i",help="Dossier contenant les fichiers Markdown")
-arguments_passes.add_argument("o",help="Dossier contenant les fichiers HTML convetis a partir de fichiers Markdown")
-arguments_passes.parse_args()#on affiche les arguments passes au programme
+#Message d aide pour chaque options (les arguments ne peuvent etre que des chaines de caracteres)
+arguments_passes.add_argument("i",type=str,help="Dossier contenant les fichiers Markdown")
+arguments_passes.add_argument("o",type=str,help="Dossier contenant les fichiers HTML convetis a partir de fichiers Markdown")
+args=arguments_passes.parse_args()#on affiche les arguments passes au programme
+print("Le dossier contenant les fichiers Mardown:",args.i)
+print("Le dossier contenant les fichiers HTML:",args.o)
